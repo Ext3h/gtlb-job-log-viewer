392dce521e15bb062ed2ce5ad11a1b4c487c24e6
Andreas Ringlstetter <andreas.ringlstetter@digitalwerk.net>
2025-01-13 17:19:58 +0100

    Reduce DOM size to better handle logs with >100k lines

--- src/Parser.js
+++ src/Parser.js
@@ -4,12 +4,17 @@ import { format } from "date-fns";
 
 import { ReactComponent as ArrowRight } from "./arrow-right.svg";
 import { ReactComponent as ArrowDown } from "./arrow-down.svg";
+import { Fragment } from "react/jsx-runtime";
 
 // const startRegex = /section_start:(?<startTimestamp>\d+):(?<sectionName>.+)\re\[0K(?<sectionHeader>.+)?/;
 const startRegex = /section_start:(?<startTimestamp>\d+):(?<sectionName>[a-zA-Z0-9-_]+)(?:\[(?<sectionOptions>[a-zA-Z0-9-_,=]+)\])?/;
 
 // const endRegex = /section_end:(?<endTimestamp>\d+):(?<sectionName>.+)\re\[0K/;
 const endRegex = /section_end:(?<endTimestamp>\d+):(?<sectionName>[a-zA-Z0-9-_]+)/;
+
+function hasStyle(part) {
+  return part.decoration || part.fg || part.bg;
+}
 
 function getStyle(part) {
   const style = {};
@@ -120,10 +125,44 @@ function Parser(props) {
   );
 }
 
-function Section(props) {
-  const [opened, toggle] = useState(props.options.includes("collapsed=true") ? false : true);
+function Line(props) {
+  const formatedText = anser.ansiToJson(props.text).map((stringPart, partIndex) => {
+    return stringPart.content ? (
+      hasStyle(stringPart) ? (
+        <span style={getStyle(stringPart)}>
+          {stringPart.content}
+        </span>
+      ) : stringPart.content
+    ) : null;
+  }).filter(Boolean);
 
-  let durationString;
+  if(formatedText.length > 0)
+  {
+    return (
+      <div
+        className="row"
+        id={`L${props.lineNumber}`}
+      >
+        <a
+          href={`#L${props.lineNumber}`}
+          className="line-number"
+        >
+          {props.lineNumber}
+        </a>
+        <div className="text" style={{'padding-left': props.indent * 2 + "ex"}}>
+          {React.createElement.apply(null, [Fragment, {}, ...formatedText])}
+        </div>
+      </div>
+    );
+  }
+  else
+  {
+    return null;
+  }
+}
+
+function SectionHeader(props) {
+  let durationString = "Not available";
   if (props.endTimestamp && props.startTimestamp) {
     const date = new Date((props.endTimestamp - props.startTimestamp) * 1000);
     const utcDate = convertDateToUTC(date);
@@ -131,7 +170,44 @@ function Section(props) {
     durationString = format(convertDateToUTC(date), template);
   }
 
+  const formatedText = anser.ansiToJson(props.text).map((stringPart, partIndex) => {
+    return stringPart.content ? (
+      hasStyle(stringPart) ? (
+        <span style={getStyle(stringPart)}>
+          {stringPart.content}
+        </span>
+      ) : stringPart.content
+    ) : null;
+  }).filter(Boolean);
+
   return (
+    <div
+      className="row section-row"
+      onClick={() => props.toggle(!props.opened)}
+      role="button"
+      id={`L${props.lineNumber}`}
+    >
+      <div className="arrow">
+        {props.opened ? <ArrowDown /> : <ArrowRight />}
+      </div>
+      <a
+        href={`#L${props.lineNumber}`}
+        className="line-number"
+      >
+        {props.lineNumber}
+      </a>
+
+      <div className="text" style={{'padding-left': props.indent * 2 + "ex"}}>
+        {React.createElement.apply(null, ['span', {}, ...formatedText])}
+        <span className="duration">{durationString}</span>
+      </div>
+    </div>
+  );
+}
+
+function Section(props) {
+  const [opened, toggle] = useState(props.options.includes("collapsed=true") ? false : true);
+    return (
     <>
       <section>
         {props.lines.map((line, lineIndex) => {
@@ -141,44 +217,13 @@ function Section(props) {
           if (sectionStart || opened) {
             if('text' in line)
             {
-              const formatedText = anser.ansiToJson(line.text).map((stringPart, partIndex) => {
-                return stringPart.content ? (
-                  <span key={partIndex} style={getStyle(stringPart)}>
-                    {stringPart.content}
-                  </span>
-                ) : null;
-              }).filter(Boolean);
-
-              if(sectionStart || formatedText.length > 0)
+              if(sectionStart)
               {
-                return (
-                  <div
-                    key={lineIndex}
-                    className={`row ${sectionStart ? "section-row" : ""}`}
-                    onClick={sectionStart ? () => toggle(!opened) : undefined}
-                    role={sectionStart ? "button" : ""}
-                    id={`L${line.lineNumber}`}
-                  >
-                    {sectionStart && (
-                      <div className="arrow">
-                        {opened ? <ArrowDown /> : <ArrowRight />}
-                      </div>
-                    )}
-                    <a
-                      href={`#L${line.lineNumber}`}
-                      className="line-number"
-                    >
-                      {line.lineNumber}
-                    </a>
-
-                    <div className="text" style={{'padding-left': (sectionStart ? props.indent - 1 : props.indent) * 2 + "ex"}}>
-                      <span>{formatedText}</span>
-                      {sectionStart && (
-                        <span className="duration">{durationString}</span>
-                      )}
-                    </div>
-                  </div>
-                );
+                return (<SectionHeader {...line} startTimestamp={props.startTimestamp} endTimestamp={props.endTimestamp} toggle={toggle} opened={opened} indent={props.indent - 1} key={lineIndex}/>)
+              }
+              else
+              {
+                return (<Line text={line.text} lineNumber={line.lineNumber} indent={props.indent} key={lineIndex}/>)
               }
             }
             else if('lines' in line)
